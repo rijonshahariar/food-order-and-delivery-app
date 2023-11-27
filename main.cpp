3bf0fc62fb3c415567c92f00d6f203dd8bf324c5
Shahariar Rijon <54738067+rijonshahariar@users.noreply.github.com>
2023-11-27 06:58:42 -0800

    Login & Registration added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 #include<process.h>
-#include<windows.h>
+#include<Windows.h>
 #include<time.h>
 #include<conio.h>
-
+#include<iostream>
 using namespace std;
 
 /*
@@ -18,11 +18,11 @@ void menuArrow(int realPosition, int arrowPosition)
 {
     if(realPosition == arrowPosition)
     {
-        printf("   >>   ");
+        printf("  >> ");
     }
     else
     {
-        printf("        ");
+        printf("     ");
     }
 }
 
@@ -53,6 +53,7 @@ void logo()
     printf("   |_|  \\___/ \\___/ \\__,_|_| |_| |_|\\___/|_| |_|_|\\_\\___|\\__, |  \n");
     gotoxy(20, 7);
     printf("                                                          |___/   \n");
+
 }
 
 /*
@@ -61,13 +62,173 @@ UI FUNCTIONS END
 =================
 */
 
+/*
+=========
+HOMEPAGE
+=========
+*/
 
-int main(){
+void homepage(){
+cout << "ok";
+}
+
+
+/*
+===================
+USER LOGIN & SIGNUP START
+===================
+*/
+
+struct User
+{
+    string email, pass, name, address;
+};
+
+struct User u[100];
+int n; //global variable for count users from userDB.txt file
+int current; // global variable for showing data on 'my profile' for particular user1
+
+void getData()
+{
+    int i;
+    FILE *fp;
+    fp = fopen("db/userdb.txt", "r");
+
+    for(i=0; fscanf(fp, "%s", u[i].email) != -1; i++)
+    {
+        fscanf(fp,"%s", u[i].name);
+        fscanf(fp,"%s", u[i].pass);
+        fscanf(fp,"%s", u[i].address);
+    }
+    n = i; //global variable declared above. Used to count total users.
+    fclose(fp);
+}
+
+// stores all data from struct user u[100] to userDB.txt file.
+void writeData()
+{
+    ofstream userFile("db/userdb.txt", ios::app);
+    if (!userFile.is_open()) {
+        cerr << "Error opening file for writing.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    for(int i = 0; i<n; i++)
+    {
+    userFile << u[i].email << "\n" << u[i].name << "\n" << u[i].pass << "\n" << u[i].address << "\n";
+    }
+    userFile.close();
+}
+
+// Login function. User type there username & pass correctly to log into there account.
+void login()
+{
+    system("cls");
+    logo();
+    string email, password;
+
+    int checkingIfUserfound = 0, index=0;
+    gotoxy(50, 10);
+    printf("  LOGIN\t\t");
+    gotoxy(50, 12);
+    printf("\t\t");
+    gotoxy(50, 14);
+    printf("\t\t\t");
+    gotoxy(50, 12);
+    printf("  EMAIL: ");
+    cin >> email;
+    gotoxy(50, 14);
+    printf("  PASSWORD: ");
+    cin >> password;
+
+    ifstream userFile("db/userdb.txt");
+
+    for(int i = 0; i < n; i++)
+    {
+        if(u[i].email == email && u[i].pass == password)
+        {
+            current = i; // here current represents the logged in user index number in userDB.txt file.
+            checkingIfUserfound = 1;
+
+            //if(u[i].role == "admin")
+            //{
+            //   adminPanel();
+            //    break;
+            //}
+           // else
+            //{
+                homepage();
+            //    break;
+            //}
+        }
+    }
+
+    if(checkingIfUserfound == 0)
+    {
+        gotoxy(52, 16);
+        printf("WRONG USERNAME OR PASSWORD! TRY AGAIN");
+        Sleep(1000);
+        login();
+    }
+
+
+}
+/// signup function used to register new users to the app. And there data stores in userDB.txt file.
+void signup()
+{
+    system("cls");
+    string email, name, address, password;
+
+    logo();
+    gotoxy(50, 10);
+    printf("  REGISTER\t\t");
+    gotoxy(50, 12);
+    printf("               ");
+    gotoxy(50, 14);
+    printf("                   ");
+    gotoxy(50, 12);
+    cout<< "  FULL NAME: ";
+    //cin.ignore(100, '\n');
+    getline(cin, name);
+    gotoxy(50, 14);
+    cout <<"  EMAIL: ";
+    getline(cin, email);
+    gotoxy(50, 16);
+    cout << "  ADDRESS: ";
+    getline(cin, address);
+    gotoxy(50, 18);
+    cout << "  PASSWORD: ";
+    getline(cin, password);
+
+    // Save user information to the database
+    u[n].email = email;
+    u[n].name = name;
+    u[n].pass = password;
+    u[n].address = address;
+
+    n++;
+    writeData();
+
+    gotoxy(50, 24);
+    printf("ACCOUNT CREATED SUCCESSFULLY...");
+    Sleep(1000);
+    login();
+}
+/*
+===================
+USER LOGIN & SIGNUP END
+===================
+*/
+
+
+int main()
+{
 
     system("cls");
-    printf("\e[?25l"); /// used to hide cursor in console
-    //getData();
+    printf("\e[?25l"); // used to hide cursor in console
     logo();
+
+    //getData();
 
     int a_position = 1, keyPressed = 0;
 
@@ -75,12 +236,12 @@ int main(){
     while(keyPressed != 13)
     {
         //system("cls");
-        gotoxy(45, 10);
+        gotoxy(42, 10);
         menuArrow(1,a_position);
-        printf("1. LOGIN\n");
-        gotoxy(45, 12);
+        printf("LOGIN NOW\n");
+        gotoxy(42, 12);
         menuArrow(2,a_position);
-        printf("2. REGISTER\n");
+        printf("CREATE ACCOUNT\n");
 
         keyPressed = getch();
         if(keyPressed == 80 && a_position != 2) a_position++;
@@ -90,10 +251,10 @@ int main(){
     switch(a_position)
     {
     case(1):
-        //login();
+        login();
         break;
     case(2):
-        //signup();
+        signup();
         break;
     default:
         printf("SYSTEM ERROR!");
